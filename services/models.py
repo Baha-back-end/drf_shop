167da439792a17a07163489ea03ae68c1808d29d
Baha-back-end <fancybax@gmail.com>
2024-04-19 00:59:22 +0500

    second commit

--- services/models.py
+++ services/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 
+from common.utils import validate_phone_number
 
 class ServiceCategory(MPTTModel):
     title = models.CharField(max_length=100,
@@ -93,3 +94,44 @@ class ProcedureCost(models.Model):
     class Meta:
         verbose_name = _("procedure cost")
         verbose_name_plural = _("procedure costs")
+# ----------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------
+class WhoPerformsProcedure(models.Model):
+    specialist = models.ForeignKey('clinics.Specialist', on_delete=models.CASCADE, verbose_name=_("specialist"),
+                                   related_name='procedures')
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, verbose_name=_("service"),
+                                related_name='who_performs_procedures')
+
+    class Meta:
+        verbose_name = _("Specialist's service")
+        verbose_name_plural = _("Specialist's services")
+
+    def __str__(self):
+        return f"Id: {self.id} - Specialist: {self.specialist.full_name} - Service: {self.service.title}"
+
+
+class OrderService(models.Model):
+    class OrderStatus(models.TextChoices):
+        NEW = 'new', _('new')
+        ACCEPTED = 'accepted', _('accepted')
+        PROGRESS = 'progress', _('progress')
+        CANCELED = 'canceled', _('canceled')
+        FINISHED = 'finished', _('finished')
+
+    phone = models.CharField(_("phone"), max_length=100,
+                             validators=[validate_phone_number])
+    full_name = models.CharField(_("full name"), max_length=100)
+    book_date = models.DateField(_("book date"))
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, verbose_name=_("service"))
+    status = models.CharField(_("status"), max_length=100, choices=OrderStatus.choices, default=OrderStatus.NEW)
+
+    class Meta:
+        verbose_name = _("order service")
+        verbose_name_plural = _("order services")
+
+    def __str__(self):
+        return self.full_name
+
+    def clean(self):
+        if self.book_date < datetime.date.today():
+            raise ValidationError(_("Book date must be greater than today"))
